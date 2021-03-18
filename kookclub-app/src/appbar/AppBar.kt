f7626d904f207c09bcb060ca08c403fbbf7e0563
LAPTOP-9I879AR9\lotus <lotus.ter.haar@topicus.nl>
2021-03-18 13:47:17 +0100

    AppBar added badge to notifications and the links with Bootstrap. Attributes are added with attrs["..."] = "..."

--- kookclub-app/src/appbar/AppBar.kt
+++ kookclub-app/src/appbar/AppBar.kt
@@ -1,22 +1,38 @@
 package appbar
 
 import react.*
-import react.dom.a
-import react.dom.nav
+import react.dom.*
 import styled.css
 import styled.styledDiv
 
 
 fun RBuilder.showAppBar() {
-    nav(classes = "navbar navbar-dark bg-primary") {
-        styledDiv {
-            css {
-                classes = mutableListOf("container-fluid")
+    nav(classes = "nav navbar-dark bg-success ") {
+
+            a("#",classes = "nav-link text-light") {
+                p(classes="h3"){
+                    +"Kookclub AT"
+                }
             }
-            a(classes = "navbar-brand") {
-                +"Navbar"
+           a("#", classes="nav-link active text-light") {
+               button(classes="btn btn-success text-light") {
+                   attrs["aria-current"] = "page"
+                   +"Aanmelden"
+               }
+            }
+        a("#", classes="nav-link") {
+            button(classes="btn btn-success text-light"){
+                    +"Notificaties "
+                span(classes="badge bg-danger text-light"){
+                    +"9"
+                }
+                span(classes="visually-hidden"){
+                    + "unread messages"
+                }
             }
         }
+        }
     }
-}
+
+
 
